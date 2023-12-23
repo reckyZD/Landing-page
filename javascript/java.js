9ee9b2d1065d6ce2a20d0e48184b43b403565643
ReckyZD <reckyzd@gmail.com>
2023-12-23 16:00:08 +0700

    add cust detail

--- javascript/java.js
+++ javascript/java.js
@@ -62,6 +62,5 @@ document.querySelector('.modal .close-icon').onclick = (e) => {
 window.onclick = (e) => {
   if (e.target === itemDetailModal) {
     itemDetailModal.style.display = 'none';
-    e.preventDefault();
   };
 };
